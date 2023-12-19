81d70cd75524ed3b38efef97dccacef56764d86b
Daniel Adenuga <ec22631@qmul.ac.uk>
2023-12-19 03:12:33 +0000

    checking the config works

--- assessment_code.py
+++ assessment_code.py
@@ -1,1 +1,7 @@
-print("Hello world")
+import pandas as pd
+import matplotlib as plt
+import numpy as np
+
+data = pd.read_csv("School_Attendance_by_Student_Group_and_District__2021-2022.csv")
+
+print(data.iloc[0])
