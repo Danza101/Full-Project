7c1ea6c0ee322f2a301dc751cd752243811de408
Daniel Adenuga <ec22631@qmul.ac.uk>
2023-12-19 22:48:54 +0000

    Finished up importing the modules and checking if they work properlly

--- assessment_code.py
+++ assessment_code.py
@@ -1,7 +1,11 @@
+#These are all the modules i plan on using in the development
 import pandas as pd
 import matplotlib as plt
 import numpy as np
 
+#First i get the data
 data = pd.read_csv("School_Attendance_by_Student_Group_and_District__2021-2022.csv")
 
-print(data.iloc[0])
+#The data is all about school attendance by student group and district from the last couple years
+#The sata comes from Data.gov which suggests to me that the data is reliably sourced because it came from a government based website
+#The first idea which comes to me is seeing if i could group the data some way depending on the school
--- test_code.py
+++ test_code.py
@@ -1,8 +1,31 @@
-def test_add():
-    if("a" == "a"):
-        return True
-    else:
-        return False
+import unittest as test
+from assessment_code import *
+
+# This method will be used to check if I have the correct table
+def check_value(value):
+    column_names = ['District code', 'District name', 'Category', 'Student group', '2021-2022 student count - year to date', '2021-2022 attendance rate - year to date', '2020-2021 student count', '2020-2021 attendance rate', '2019-2020 student count', '2019-2020 attendance rate', 'Reporting period', 'Date update']
+    return column_names
+
+#This will be used to import the moduels from assessment code file
+def modulesInstalled():
+    try:
+        from assessment_code import pd
+        from assessment_code import plt
+        from assessment_code import np
+        return(True)
+    except ImportError:
+        return(False)
     
     
-assert test_add() == True
+# Requirements 1: Testing I have imported the correct imports to this file
+class Test_Imports(test.TestCase):
+    def test_data_import(self):
+        # This checks that the data I am importing in the coding file is the same as the data I am using in this file
+        self.assertEqual(data.columns.tolist(), check_value(0))  
+    
+    def test_modules_imports(self):
+        #This code tests that pandas, matplotlib and numpy has been downloaded properlly
+        self.assertEqual(True , modulesInstalled())
+
+if __name__ == '__main__':
+    test.main()
