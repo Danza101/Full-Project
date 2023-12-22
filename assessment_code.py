d33d80f8b07502156a34f0995aa1dc6c4be3665d
Daniel Adenuga <ec22631@qmul.ac.uk>
2023-12-22 04:39:11 +0000

    Loading a new csv file into the vscode

--- assessment_code.py
+++ assessment_code.py
@@ -1,11 +1,19 @@
 #These are all the modules i plan on using in the development
 import pandas as pd
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import numpy as np
 
 #First i get the data
-data = pd.read_csv("School_Attendance_by_Student_Group_and_District__2021-2022.csv")
+data = pd.read_csv("Global_Education.csv" , encoding='ISO-8859-1')
 
 #The data is all about school attendance by student group and district from the last couple years
 #The sata comes from Data.gov which suggests to me that the data is reliably sourced because it came from a government based website
-#The first idea which comes to me is seeing if i could group the data some way depending on the school
+#From this data the best thing i will be able to study would be how different levels of education affect the likelyhood of getting
+#a job depending on the country
+
+# Assuming 'data' is a pandas DataFrame with the correct columns.
+country = data["Countries and areas"]  # This should be a Series or list of country names.
+unemployment = data["Unemployment_Rate"].astype(float)  # This should be a Series of floats.
+
+plt.bar(country[:10], unemployment[:10] , rotation='vertical')  # Using a bar chart for categorical x-axis data.
+plt.show()  # To display the plot.
--- test_code.py
+++ test_code.py
@@ -1,12 +1,17 @@
+import random 
 import unittest as test
 from assessment_code import *
 
-# This method will be used to check if I have the correct table
-def check_value(value):
-    column_names = ['District code', 'District name', 'Category', 'Student group', '2021-2022 student count - year to date', '2021-2022 attendance rate - year to date', '2020-2021 student count', '2020-2021 attendance rate', '2019-2020 student count', '2019-2020 attendance rate', 'Reporting period', 'Date update']
-    return column_names
+def test_data_function(data, data_checker):
+    for _ in range(20):
+        random_index = random.randrange(1, 25)
+        if not data.iloc[random_index].equals(data_checker.iloc[random_index]):
+            return False
+    return True
 
+    
 #This will be used to import the moduels from assessment code file
+#It tries to downloads all the imports from the assessment_code file and if it isnt able it retuens the error
 def modulesInstalled():
     try:
         from assessment_code import pd
@@ -19,10 +24,11 @@ def modulesInstalled():
     
 # Requirements 1: Testing I have imported the correct imports to this file
 class Test_Imports(test.TestCase):
-    def test_data_import(self):
-        # This checks that the data I am importing in the coding file is the same as the data I am using in this file
-        self.assertEqual(data.columns.tolist(), check_value(0))  
     
+    def test_data_import(self):
+        data_checker = pd.read_csv("Global_Education.csv", encoding='ISO-8859-1')
+        self.assertEqual(test_data_function(data , data_checker) , True)
+        
     def test_modules_imports(self):
         #This code tests that pandas, matplotlib and numpy has been downloaded properlly
         self.assertEqual(True , modulesInstalled())
