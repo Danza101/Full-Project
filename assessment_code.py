8855deefb26e6cced8dcdf5c5140ac9b0db724de
Daniel Adenuga <ec22631@qmul.ac.uk>
2023-12-22 04:53:08 +0000

    Fixed issue where the graph was creating an error

--- assessment_code.py
+++ assessment_code.py
@@ -15,5 +15,6 @@ data = pd.read_csv("Global_Education.csv" , encoding='ISO-8859-1')
 country = data["Countries and areas"]  # This should be a Series or list of country names.
 unemployment = data["Unemployment_Rate"].astype(float)  # This should be a Series of floats.
 
-plt.bar(country[:10], unemployment[:10] , rotation='vertical')  # Using a bar chart for categorical x-axis data.
+plt.bar(country[:10], unemployment[:10])  # Using a bar chart for categorical x-axis data.
+plt.xticks(range(len(country[:10])), country[:10], rotation='vertical')
 plt.show()  # To display the plot.
