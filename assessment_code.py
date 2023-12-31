ee7ee90e53ed292ebc9aa8a9db0cd229b51577c8
Daniel Adenuga <ec22631@qmul.ac.uk>
2023-12-31 00:05:10 +0000

    This should be the unit tests for the heat map of the world

--- assessment_code.py
+++ assessment_code.py
@@ -3,8 +3,11 @@ import pycountry_convert as pc
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-import geopandas as gp
-
+import warnings
+import geopandas as gpd
+import cartopy.crs as ccrs
+import contextily as ctx
+ 
 def converting_country_to_continent(country_name):
     try:
         country_alpha2 = pc.country_name_to_country_alpha2(country_name)
@@ -19,28 +22,70 @@ def converting_country_to_continent(country_name):
             country_continent_name = 'Asia'
 
     return country_continent_name
+
+class getting_the_world_map():
     
+    def __init__(self, plot_data):
+        self.plot_data = plot_data
+    
+    def getting_the_data(self):
+        #First i get the data
+        data = pd.read_csv("Global_Education.csv" , encoding='ISO-8859-1')
+        
+        # Filter out specific warnings from the pycountry library
+        warnings.filterwarnings("ignore", message="Country's official_name not found. Country name provided instead.")
+        warnings.filterwarnings("ignore", message="Country's common_name not found. Country name provided instead.")
+        
+        # Assuming 'data' is a pandas DataFrame with the correct columns.
+        country = data["Countries and areas"]  # This should be a Series or list of country names.
+        unemployment = data["Unemployment_Rate"].astype(float)  # This should be a Series of floats.
+        continents = (country.apply(converting_country_to_continent)) #This gets the continents for each country
+        
+        return country , unemployment , continents , data
+
+    def manipulating_the_data(self , country , unemployment , continents):
+        #Merging the continent and the unemployment rrate to one dataframe
+        continent_Unemployment_merged = pd.concat([continents , unemployment] , axis = 1) 
+
+        # Ensure the merged DataFrame has appropriate column names
+        continent_Unemployment_merged.columns = ['continent', 'Unemployment_Rate']
+
+        # Grouping the data by the 'Continent' column
+        grouped_data = continent_Unemployment_merged.groupby('continent')
+
+        # Calculating the mean unemployment rate per continent
+        mean_unemployment_per_continent = grouped_data.mean()
+
+        # Reset the index to convert 'Continent' from an index to a column
+        mean_unemployment_per_continent = mean_unemployment_per_continent.reset_index()
+
+        #Changing the datatype to be a dictionary data type
+        unemployment_dict = dict(zip(mean_unemployment_per_continent['continent'], mean_unemployment_per_continent['Unemployment_Rate']))
+        
+        return unemployment_dict
+
+    def printing_the_data(self , unemployment_dict):
+        # Load the shapefile
+        world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
+
+        #This gets each country from world data and 
+        world['Data'] = world['continent'].map(unemployment_dict)
+
+        #This makes sure the graph only shows if the code is cqalled from this 
+        if self.plot_data:
+            # Create a plot
+            fig, ax = plt.subplots(figsize=(12, 6))
+            world.plot(ax=ax, column='Data', legend=True, cmap='OrRd')
+
+            # Add a basemap
+            ctx.add_basemap(ax, crs=world.crs.to_string())
+
+            #This displays the heat map of average unemployment in different countries
+            plt.show()
     
 
-#First i get the data
-data = pd.read_csv("Global_Education.csv" , encoding='ISO-8859-1')
-
-# Assuming 'data' is a pandas DataFrame with the correct columns.
-country = data["Countries and areas"]  # This should be a Series or list of country names.
-unemployment = data["Unemployment_Rate"].astype(float)  # This should be a Series of floats.
-continents = (country.apply(converting_country_to_continent)) #This gets the continents for each country
-
-#Merging the continent and the unemployment rrate to one dataframe
-continent_Unemployment_merged = pd.concat([continents , unemployment] , axis = 1) 
-
-# Ensure the merged DataFrame has appropriate column names
-continent_Unemployment_merged.columns = ['Continent', 'Unemployment_Rate']
-
-# Grouping the data by the 'Continent' column
-grouped_data = continent_Unemployment_merged.groupby('Continent')
-
-# Calculating the mean unemployment rate per continent
-mean_unemployment_per_continent = grouped_data.mean()
-
-
-
+if __name__ == "__main__":
+    world_map = getting_the_world_map(plot_data=True)
+    country, unemployment, continents , data = world_map.getting_the_data()  # 'self' is passed implicitly
+    unemployment_dict = world_map.manipulating_the_data(country, unemployment, continents)  # 'self' is passed implicitly
+    world_map.printing_the_data(unemployment_dict)  # 'self' is passed implicitly, 'plot_data' is accessed via 'self.plot_data'
--- test_code.py
+++ test_code.py
@@ -1,19 +1,56 @@
-import random 
+import random
+import pandas as pd
+import awoc 
 import unittest as test
-from assessment_code import *
+import assessment_code as ac
+
+def checking_the_continent(country):
+    my_world = awoc.AWOC()
+    continent_names = ['Europe', 'Africa', 'Oceania', 'North America', 'South America']
+    
+    # Create a dictionary to hold the countries with their respective continent
+    continent_dict = {}
+    for name in continent_names:
+        countries = my_world.get_countries_list_of(name)
+        for c in countries:
+            continent_dict[c] = name
+    
+    # Find the continent for the given country
+    return continent_dict.get(country, "Unknown")
+
+def getting_the_data_from_assessment_code():
+    
+    try:
+        # Create an instance of the class
+        world_map = ac.getting_the_world_map(plot_data=False)
+
+        # Call the instance method and capture its returned data
+        country, unemployment, continents , data = world_map.getting_the_data()
+        
+        return country , unemployment , continents , data
+    except Exception as e:
+        # Return the error message for debugging purposes
+        return f"There is an error: {e}"
 
 #This code is used to check if the columns in the dataset are the ones you expect
 def test_data_function(data, data_checker):
-    for _ in range(20):
-        random_index = random.randrange(1, 25)
-        if not data.iloc[random_index].equals(data_checker.iloc[random_index]):
+    try:
+        # Check if the data is the same length
+        if len(data) != len(data_checker):
             return False
-    return True
 
-    
+        # If the data is the same length, check all of the indexes match
+        for i in range(len(data)):
+            if not data.iloc[i].equals(data_checker.iloc[i]):
+                return False
+        return True
+    except Exception as e:
+        # Return the error message for debugging purposes
+        return f"There is an error: {e}"
+
 #This will be used to import the moduels from assessment code file
 #It tries to downloads all the imports from the assessment_code file and if it isnt able it retuens the error
-def modulesInstalled():
+def checking_modules_installed():
     try:
         from assessment_code import pd
         from assessment_code import plt
@@ -21,11 +58,38 @@ def modulesInstalled():
         return(True)
     except ImportError:
         return(False)
-    
+
+# This is a function which uses anoother class to get all the countries in each continent   
+def checking_correct_continent(country , continents , country_data_checker):
+   
+   #The first thing i need to do is find the continents which each the country data checker belongs to
+    continents_data_checker = country_data_checker.apply(checking_the_continent)
+
+    #Next thing is to check that the country matches the data needed
+    try:
+        for i in range(len(country)):
+            #This checks the country i used in the dowmloaded file is the same as the county data in my coding file
+            if(country[i] != country_data_checker[i]):
+                print(country[i] + "||" + country_data_checker[i])
+                return False
+            #This checks if the country in the contines expected
+            if (continents[i] != continents_data_checker[i]):
+                if (continents_data_checker[i] == ("unknown")):
+                    print(continents[i] + "||" + continents_data_checker[i])
+                    return False
+        return True    
+    except Exception as e:
+        # Return the error message for debugging purposes
+        print("There is an error")
+        return f"There is an error: {e}"    
     
 # Requirements 1: Testing I have imported the correct imports to this file
 class Test_Imports(test.TestCase):
     
+    def setUp(self):
+        # Common setup for each test method in this class
+        self.country, self.unemployment, self.continents, self.data = getting_the_data_from_assessment_code()
+        
     #calls method to check the data is good 
     def test_data_import(self):
         data_checker = pd.read_csv("Global_Education.csv", encoding='ISO-8859-1')
@@ -33,11 +97,25 @@ class Test_Imports(test.TestCase):
         
     def test_modules_imports(self):
         #This code tests that pandas, matplotlib and numpy has been downloaded properlly
-        self.assertEqual(True , modulesInstalled())
+        self.assertEqual(True , checking_modules_installed())
 
-class Test_Graphs(test.TestCase):
-    #This code checks if i have split the data into the correct continents
-    print("Hello Woels")
-    
+#Requirements 2: Testing that each country has gone to the correct continent
+class TestGraphs(test.TestCase):
+
+    def setUp(self):
+        # Common setup for each test method in this class
+        self.country, self.unemployment, self.continents, self.data = getting_the_data_from_assessment_code()
+
+    def test_correct_continent(self):
+        # When testing, suppress plotting
+        data_checker = pd.read_csv("Global_Education.csv", encoding='ISO-8859-1')["Countries and areas"]
+        self.assertEqual(True , checking_correct_continent(country , continents , data_checker))
+        
+
+
 if __name__ == '__main__':
+    #Getting the requires data i need from my assessment code file
+    country , unemployment, continents , data = getting_the_data_from_assessment_code()
+    
+    #Checking the continent
     test.main()
